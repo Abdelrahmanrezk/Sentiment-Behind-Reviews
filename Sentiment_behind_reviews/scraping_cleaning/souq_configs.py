c72c05683deae53cb2d4c17eb40e607a0e81499f
Abdelrahmanh Rezk <abdelrahmanhamdy33@gmail.com>
2020-03-20 06:38:55 +0200

    update the files

--- Sentiment_behind_reviews/scraping_cleaning/souq_configs.py
+++ Sentiment_behind_reviews/scraping_cleaning/souq_configs.py
@@ -5,8 +5,10 @@ gecko_driver = '../driver/geckodriver'
 headless = False
 all_pro_reviews = [] # this used for get all reviews of all products we scrapping
 import pymongo
+
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
-mongo_url = "cluster0-uedg8.mongodb.net/test?retryWrites=true&w=majority"
-mongo_user = "Abdelrahman_Rezk"
-mongo_pass = "Abdelrahman_12011"
+# apply your data
+# mongo_url = 
+# mongo_user =
+# mongo_pass =
 
